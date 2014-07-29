01859538188e1784782a7b2a1073324c2568279f
sepherro <marek.kraft@gmail.com>
2014-07-29 12:57:59 +0200

    Started the work on multiple camera support, need to cram the camera states into a single list depending on data header

--- server.py
+++ server.py
@@ -1,17 +1,19 @@
-from pydoc import serve
 import socket
 import cv2
 import numpy as np
 import threading
 from pyqtgraph.Qt import QtGui, QtCore
-import numpy as np
 import pyqtgraph as pg
+import time
+
+# import pyqtgraph.examples
+# pyqtgraph.examples.run()
 
 ### global variables ###
 #white pixel percentage
-percentage = 0
+percentage = np.zeros((4, 1))
 #activation level history
-activation_history = [0] * 200
+activation_history = np.zeros((4, 200))
 decimg = []
 
 ### connection details ###
@@ -23,10 +25,11 @@ def recvall(sock, count):
     buf = b''
     while count:
         newbuf = sock.recv(count)
-        #TODO: it may be ebough to comment the line below
-        if not newbuf: return None
-        buf += newbuf
-        count -= len(newbuf)
+        if newbuf is not None:
+            buf += newbuf
+            count -= len(newbuf)
+        else:
+            time.sleep(0.001)
     return buf
 
 def incoming():
@@ -48,42 +51,68 @@ def incoming():
 def service_clients(clientsocket, clientaddr):
     global percentage, activation_history, decimg
     while True:
-        percentage = float(recvall(clientsocket, 16))
-        activation_level = float(recvall(clientsocket, 16))
+        node = recvall(clientsocket, 32)
+        percentage[0] = float(recvall(clientsocket, 32))
+        activation_level = float(recvall(clientsocket, 32)) + 1
         #receive the length of the image payload then pull the whole image through the socket
-        length = recvall(clientsocket, 16)
-        # TODO: right now all data is received (according to length), but there's no warranty that all has been sent
-        # TODO: recvall returns none in such case
+        length = recvall(clientsocket, 32)
         string_data = recvall(clientsocket, int(length))
         data = np.fromstring(string_data, dtype='uint8')
         #decode jpg image to numpy array and display
         decimg = cv2.imdecode(data, 1)
         if(decimg != None):
             cv2.imshow('SERVER', decimg)
-        activation_history.append(activation_level)
-        del activation_history[0]
+        activation_history[0] = np.roll(activation_history[0], -1)
+        activation_history[0][199] = activation_level
         key = cv2.waitKey(1)
 
 def update_plot_1():
     global curve_1, bar_1, cam_plot_1
-    curve_1.setData(activation_history)
-    bar_1.setData([0, 200], [percentage])
-
-#table holding a 200-sample history of activation level
-activation_history = [0] * 200
+    curve_1.setData(activation_history[0])
+    bar_1.setData([0, 200], percentage[0])
 
 # set default background color to white
 pg.setConfigOption('background', 'w')
 # open the plot window, set properties
 win = pg.GraphicsWindow(title="VSN activity monitor")
-win.resize(1000, 600)
+win.resize(1400, 800)
 win.setWindowTitle('VSN activity monitor')
 
-#setup the plot
+#setup plot 1
 cam_plot_1 = win.addPlot(title="picam01")
 curve_1 = cam_plot_1.plot(pen='r')
-bar_1 = pg.PlotCurveItem([0, 200], [percentage], stepMode=True, fillLevel=0, brush=(0, 0, 255, 20))
+bar_1 = pg.PlotCurveItem([0, 200], [0], stepMode=True, fillLevel=0, brush=(0, 0, 255, 20))
 cam_plot_1.addItem(bar_1)
+#set the scale of the plot
+cam_plot_1.setYRange(0, 300)
+
+#setup plot 2
+cam_plot_2 = win.addPlot(title="picam02")
+curve_2 = cam_plot_2.plot(pen='r')
+bar_2 = pg.PlotCurveItem([0, 200], [0], stepMode=True, fillLevel=0, brush=(0, 0, 255, 20))
+cam_plot_2.addItem(bar_2)
+#set the scale of the plot
+cam_plot_2.setYRange(0, 300)
+
+#next row
+win.nextRow()
+
+#setup plot 3
+cam_plot_3 = win.addPlot(title="picam03")
+curve_3 = cam_plot_3.plot(pen='r')
+bar_3 = pg.PlotCurveItem([0, 200], [0], stepMode=True, fillLevel=0, brush=(0, 0, 255, 20))
+cam_plot_3.addItem(bar_3)
+#set the scale of the plot
+cam_plot_3.setYRange(0, 300)
+
+#setup plot 3
+cam_plot_4 = win.addPlot(title="picam04")
+curve_4 = cam_plot_4.plot(pen='r')
+bar_4 = pg.PlotCurveItem([0, 200], [0], stepMode=True, fillLevel=0, brush=(0, 0, 255, 20))
+cam_plot_4.addItem(bar_4)
+#set the scale of the plot
+cam_plot_4.setYRange(0, 300)
+
 
 timer_plot_1 = QtCore.QTimer()
 timer_plot_1.timeout.connect(update_plot_1)
@@ -95,6 +124,3 @@ if __name__ == '__main__':
     server_thread.start()
     #thread.start_new_thread(incoming)
     QtGui.QApplication.instance().exec_()
-    print "Do I even get here?"
-    print decimg
-
