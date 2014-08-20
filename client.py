81c466603c8771173d1bde2c4a51132cc1004f99
mkr <marek.kraft@gmail.com>
2014-08-20 14:48:34 +0200

    minor modifications to the client code, starting the work in the threaded version

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ import numpy
 import cv2.cv as cv
 import math
 import time
+import threading
 
 TCP_IP = '192.168.0.100'
 TCP_PORT = 5001
@@ -18,32 +19,35 @@ capture.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
 for x in range(0, 15):
     ret, frame = capture.read()
 background = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-struct = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+struct = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 
-print "Frame resolution set to: (" + str(capture.get(cv.CV_CAP_PROP_FRAME_WIDTH)) + "; " + str(capture.get(cv.CV_CAP_PROP_FRAME_HEIGHT)) + ")"
+print "Frame resolution set to: (" + str(capture.get(cv.CV_CAP_PROP_FRAME_WIDTH)) + "; " + str(
+    capture.get(cv.CV_CAP_PROP_FRAME_HEIGHT)) + ")"
 print "Client running, press ESC to quit"
 
-activation_level_past = 0
+filename = "log.csv"            # log file name - if logging is enabled
+activation_level_past = 0       # default starting activation level
+sample_time = 0.1               # sample time at startup
+flag_senddata = False           # default behavior - do not send the image data
 
-def lowpass(prev_sample, input):
+def lowpass(prev_sample, input_val, sample_time):
     gain = 0.1
     time_constant = 1
-    sample_time = 0.1
-    output = (gain/time_constant) * input + prev_sample * pow(math.e, -1.0 *(sample_time/time_constant))
+    output = (gain / time_constant) * input_val + prev_sample * pow(math.e, -1.0 * (sample_time / time_constant))
     return output
 
 while True:
 
     # simple profiling
-    start = time.clock()
+    # start = time.clock()
 
     ret, frame = capture.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     mask_gt = cv2.compare(background, gray, cv2.CMP_GT)
     mask_lt = cv2.compare(background, gray, cv2.CMP_LT)
-    background += mask_lt/128
-    background -= mask_gt/128
+    background += mask_lt / 128
+    background -= mask_gt / 128
     difference = cv2.absdiff(background, gray)
     difference = cv2.medianBlur(difference, 3)
     display = cv2.compare(difference, 6, cv2.CMP_GT)
@@ -54,30 +58,33 @@ while True:
     height, width = dilated.shape[:2]
     percentage = (nonzero * 100 / (height * width))
 
-    activation_level = lowpass(activation_level_past, percentage)
+    activation_level = lowpass(activation_level_past, percentage, sample_time)
     activation_level_past = activation_level
 
-    encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
     result, imgencode = cv2.imencode('.jpg', dilated, encode_param)
     data = numpy.array(imgencode)
-   
+
     stringData = data.tostring()
-    sock.send( socket.gethostname().ljust(8) )
-    sock.send( str(percentage).ljust(32) )
-    sock.send( str(activation_level).ljust(32) )
-    sock.send( str(len(stringData)).ljust(32) )
-    sock.send( stringData )
+    sock.send(socket.gethostname().ljust(8))
+    sock.send(str(percentage).ljust(32))
+    sock.send(str(activation_level).ljust(32))
+    sock.send(str(len(stringData)).ljust(32))
+    sock.send(stringData)
 
     key = cv2.waitKey(10)
-    if key == 27:               # exit on ESC
+    if key == 27:  # exit on ESC
         break
 
-    print "Elapsed time: ", time.clock() - start , "seconds"
+    # write debug info to a file
+    # filehandle = open(filename, 'a')
+    # filehandle.write(str(time.clock() - start) + "\r\n")
+    # filehandle.close()
 
 sock.close()
 
 cv2.waitKey(0)
-cv2.destroyAllWindows() 
+cv2.destroyAllWindows()
 
 print "Client terminated"
 
