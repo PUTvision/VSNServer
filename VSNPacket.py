3f5155b467f1b56f0f556697feb1a20cb2d4cdf4
Michal Fularz <michal.fularz@gmail.com>
2015-04-19 19:14:56 +0200

    Added new packet type for sending parameters to clients (not used yet).

--- VSNPacket.py
+++ VSNPacket.py
@@ -59,7 +59,6 @@ class VSNPacketImage:
 
 
 class VSNPacketToClient:
-
     def __init__(self):
         # https://docs.python.org/2/library/struct.html
         self._struct_format = "! f 2s ?"
@@ -80,3 +79,55 @@ class VSNPacketToClient:
 
     def unpack_from_receive(self, string):
         self.activation_neighbours, self.image_type, self.flag_send_image = self._s.unpack(string)
+
+
+class VSNPacketToClientParameters:
+    def __init__(self):
+        self._struct_format = "! 2s ? f f f f f"
+        self._prefixLength = struct.calcsize(self._struct_format)
+        self._s = struct.Struct(self._struct_format)
+
+        self.image_type = IMAGE_TYPES.foreground
+        self.flag_send_image = False
+        self.threshold = 10.0
+        self.gain_below_threshold = 2.0
+        self.sample_time_below_threshold = 1.0
+        self.gain_above_threshold = 0.1
+        self.sample_time_above_threshold = 0.1
+
+    def set(self, image_type,
+            flag_send_image,
+            threshold,
+            gain_below,
+            sample_time_below,
+            gain_above,
+            sample_time_above
+            ):
+        self.image_type = image_type
+        self.flag_send_image = flag_send_image
+        self.threshold = threshold
+        self.gain_below_threshold = gain_below
+        self.sample_time_below_threshold = sample_time_below
+        self.gain_above_threshold = gain_above
+        self.sample_time_above_threshold = sample_time_above
+
+    def pack_to_send(self):
+        return self._s.pack(
+            self.image_type,
+            self.flag_send_image,
+            self.threshold,
+            self.gain_below_threshold,
+            self.sample_time_below_threshold,
+            self.gain_above_threshold,
+            self.sample_time_above_threshold
+        )
+
+    def unpack_from_receive(self, string):
+        self.image_type, \
+        self.flag_send_image, \
+        self.threshold, \
+        self.gain_below_threshold, \
+        self.sample_time_below_threshold, \
+        self.gain_above_threshold, \
+        self.sample_time_above_threshold \
+            = self._s.unpack(string)
