0b59cadd5291487febf14b3346b19912c8bc8d0a
Dominik Pieczyński <dominik.pieczynski@gmail.com>
2016-03-15 12:41:37 +0100

    Add packages autodiscovery in setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from vsn_server import __version__
 
 setup(
-      name='VSN',
-      version=__version__,
-      description='Network of smart cameras with enhanced autonomy ui',
-      author='PUT Vision Lab',
-      url='https://github.com/PUTvision/VSNServer',
-      install_requires=[
-          'PyYAML',
-          'quamash',
-          'git+https://github.com/pyqtgraph/pyqtgraph.git@develop',
-          'numpy',
-      ],
-      packages=['vsn_server.ui', 'vsn_server.common', 'vsn_server.connectivity'],
-      scripts=['bin/VSNServer', 'bin/VSNHistoryPlotter', 'bin/VSNUpdater'],
+    name='VSN',
+    version=__version__,
+    description='Server for network of smart cameras with enhanced autonomy',
+    author='PUT Vision Lab',
+    url='https://github.com/PUTvision/VSNServer',
+    install_requires=[
+        'PyYAML',
+        'quamash',
+        'git+https://github.com/pyqtgraph/pyqtgraph.git@develop',
+        'numpy',
+    ],
+    packages=find_packages(),
+    scripts=['bin/VSNServer', 'bin/VSNHistoryPlotter'],
 )
